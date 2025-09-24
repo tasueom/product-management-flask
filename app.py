ec2062ef8c688e4e4b31821365f78c659208f4c3
tasueom <tasueom@naver.com>
2025-09-24 14:21:55 +0900

    feat: DB 연결하고 conn과 커서 반환하는 함수와 최초 실행 시 테이블 생성 함수

--- app.py
+++ app.py
@@ -5,5 +5,33 @@ import hashlib
 app = Flask(__name__)
 app.secret_key = "secret_key_123"
 
+def init_db():
+    conn, cur = conn_db()
+    cur.execute("""
+                create table if not exists users(
+                    uid integer primary key autoincrement,
+                    username text not null,
+                    email text not null,
+                    password text not null,
+                    role text not null default 'user'
+                )
+                """)
+    cur.execute("""
+                create table if not exists products(
+                    pid integer primary key autoincrement,
+                    name text not null,
+                    price integer not null,
+                    stock integer not null,
+                    description text
+                )
+                """)
+
+def conn_db():
+    conn = sqlite3.connect("product-management-flask/database.db")
+    cur = conn.cursor()
+    
+    return conn, cur
+
 if __name__ == "__main__":
+    init_db()
     app.run(debug=True)
